bf5324eb1e7be4e7ee0bd07a3104163a5afe5e81
Haz Benz <Hazbenz@users.noreply.github.com>
2016-08-04 04:46:50 -0400

    Update Living.java

--- Gui_Worldlife/Living.java
+++ Gui_Worldlife/Living.java
@@ -1,5 +1,5 @@
 /**
- * Created by Maureen Hanna on 05/15/16.
+ * Created by Hasna Benzekri on 05/15/16.
  */
 public interface Living{
     //Every living thing must know if there alive including Plants
